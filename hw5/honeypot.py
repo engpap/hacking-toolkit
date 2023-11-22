924e7175b2c32164b5c341a81eeeeba63b44a3c3
engpap <10701904@polimi.it>
2023-11-21 23:53:44 -0600

    honeypot with filesystem

--- hw5/honeypot.py
+++ hw5/honeypot.py
@@ -23,6 +23,9 @@ NUM_ATTEMPTS = 1
 SERVER_KEY_PATH = '/Users/dre/Desktop/NetSecurity/homeworks/cs468/hw5/server.key'
 IDLE_TIMEOUT = 60  # seconds
 
+# Global variable for the simulated file system
+file_system = {'/': {}}  # Root directory is empty initially
+
 # Dictionary to store login attempts
 login_attempts = {}
 
@@ -77,11 +80,63 @@ class SSHHoneypot(paramiko.ServerInterface):
 
 
 def process_command(channel, command, username):
-    if command == 'exit':
-        channel.send('\r\nGoodbye!\n')
-        return True
-    channel.send(f'\r\nCommand {command} executed\n')
+    global file_system
+    parts = command.split()
+    debugPrint(f"\nReceived command parts: {parts}")
+
+    def is_valid_filename(filename):
+        return filename.endswith('.txt')
+
+    if parts[0] == 'ls':
+        current_files = ' '.join(file_system['/'].keys())
+        channel.send(f'\r\n{current_files}\n')
+        debugPrint("Executed ls command")
+
+    elif parts[0] == 'echo' and '>' in command:
+        _, content, _, filename = command.split()
+        if not is_valid_filename(filename):
+            channel.send(f'\r\nUnknown file extension\n')
+            debugPrint(f"Unknown file extension error for filename: {filename}")
+            return False
+        content = content.strip("''")  # Remove extra quotes
+        file_system['/'][filename] = content
+        channel.send(f'\r\nFile {filename} created\n')
+        debugPrint(f"File created: {filename} with content: {content}")
+
+    elif parts[0] == 'cat':
+        filename = parts[1]
+        if not is_valid_filename(filename):
+            channel.send(f'\r\nUnknown file extension\n')
+            debugPrint(f"Unknown file extension error for filename: {filename}")
+            return False
+        if filename in file_system['/']:
+            channel.send(f'\r\n{file_system["/"][filename]}\n')
+            debugPrint(f"Displayed content of file: {filename}")
+        else:
+            channel.send(f'\r\nFile {filename} not found\n')
+            debugPrint(f"File not found error for filename: {filename}")
+
+    elif parts[0] == 'cp':
+        src, dest = parts[1], parts[2]
+        if not all(is_valid_filename(f) for f in [src, dest]):
+            channel.send(f'\r\nUnknown file extension\n')
+            debugPrint(f"Unknown file extension error for filenames: {src}, {dest}")
+            return False
+        if src in file_system['/']:
+            file_system['/'][dest] = file_system['/'][src]
+            channel.send(f'\r\nFile {dest} created with content from {src}\n')
+            debugPrint(f"Copied content from {src} to {dest}")
+        else:
+            channel.send(f'\r\nFile {src} not found\n')
+            debugPrint(f"File not found error for source file: {src}")
+
+    else:
+        channel.send(f'\r\nCommand {command} executed\n')
+        debugPrint(f"Executed custom command: {command}")
+        return False
+
     return False
+
 
 def handle_client(client):
     transport = paramiko.Transport(client)
@@ -114,20 +169,30 @@ def handle_client(client):
         command_buffer = ''  # Buffer to accumulate command characters
 
         while True:
-            if time.time() - last_active > IDLE_TIMEOUT:
-                print("Connection idle for too long. Disconnecting.")
-                break
+            try:
+                if time.time() - last_active > IDLE_TIMEOUT:
+                    print("Connection idle for too long. Disconnecting.")
+                    break
 
-            if channel.recv_ready():
-                char = channel.recv(1).decode('utf-8')  # Read one character at a time
-                if char == '\r' or char == '\n':  # Check for carriage return or newline
-                    if command_buffer.strip():  # If there's a command in the buffer
-                        process_command(channel, command_buffer, server.username)
-                        last_active = time.time()  # Reset the activity timer
-                        command_buffer = ''  # Clear the buffer for the next command
-                    channel.send(f'\r\n{server.username}@honeypot:/$ ')  # Send prompt for next command
-                else:
-                    command_buffer += char  # Add character to buffer
+                if channel.recv_ready():
+                    char = channel.recv(1).decode('utf-8')  # Read one character at a time
+
+                    if char == '\x7f' or char == '\x08':  # Handle backspace/delete key
+                        if command_buffer:
+                            command_buffer = command_buffer[:-1]  # Remove last character from buffer
+                            channel.send('\b \b')  # Move cursor back, overwrite with space, then move back again
+                    elif char == '\r' or char == '\n':  # Carriage return or newline
+                        if command_buffer.strip():  # If there's a command in the buffer
+                            process_command(channel, command_buffer, server.username)
+                            last_active = time.time()  # Reset the activity timer
+                            command_buffer = ''  # Clear the buffer for the next command
+                        channel.send(f'\r\n{server.username}@honeypot:/$ ')  # Send prompt for next command
+                    else:
+                        command_buffer += char  # Add character to buffer
+                        channel.send(char)  # Optionally echo back the character
+            except socket.error as e:
+                print(f'Socket exception: {e}')
+                break  # Break out of the loop in case of socket error
 
             time.sleep(0.1)  # Prevents high CPU usage in the loop
     except Exception as e:
@@ -135,6 +200,7 @@ def handle_client(client):
     finally:
         channel.close()
         client.close()
+
 
 def start_server(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
